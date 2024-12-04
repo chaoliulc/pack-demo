132869115ac28edd7f8758ae825e364448d7cba7
chaoliulc <chaoliulc@foxmail.com>
2024-12-04 18:55:19 +0800

    refactor: remove mjs & cjs

--- src/cjs/index.cjs
+++
@@ -1,5 +0,0 @@
-const util = require("./util.js");
-exports.default = util;
-exports.add = util.add;
-exports.subtract = util.subtract;
-exports.testObj = util.testObj;
--- src/esm/index.mjs
+++
@@ -1,5 +0,0 @@
-import cjsModule from "../cjs/index.js";
-export const add = cjsModule.add;
-export const subtract = cjsModule.subtract;
-export const testObj = cjsModule.testObj;
-export default cjsModule;
